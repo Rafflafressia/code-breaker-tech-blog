503702f0225d84fc3526880bd8c09fbbcfe716ff
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-04-07 15:51:19 -0400

    Fixed render bug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
             include: [
                 {
                     model: User,
-                    attributes: ['name'],
+                    attributes: ['username'],
                 },
                 {
                     model: Comment,
@@ -25,7 +25,7 @@ router.get('/', async (req, res) => {
         // Pass serialized data and session flag into template
         res.render('homepage', { 
             posts, 
-            logged_in: req.session.logged_in 
+            // logged_in: req.session.logged_in 
         });
 
     } catch (err) {
