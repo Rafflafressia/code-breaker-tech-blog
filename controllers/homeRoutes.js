24bfebb34fec13c94d80c7bed96a2a8a83cb5cb9
Rafflafressia <149746567+Rafflafressia@users.noreply.github.com>
2024-04-07 15:03:40 -0400

    Updated Home Route, added withAuth utility - debugging homepage render bug

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,37 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/withAuth');
 
 router.get('/', async (req, res) => {
     try {
-        res.render('homepage');
-    } catch {
-        res.status(500).json(err);
+
+        // Get all posts and JOIN with user data
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['comment_text'],
+                }
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const posts = postData.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', { 
+            posts, 
+            logged_in: req.session.logged_in 
+        });
+
+    } catch (err) {
+
+        res.status(500).json({message: "An error occurred. Please try again later."});
+
     }
 });
 
---
+++ utils/withAuth.js
@@ -0,0 +1,11 @@
+// Create an auth HOC to protect routes
+
+const withAuth = (req, res, next) => {
+    if (!req.session.logged_in) {
+        res.redirect('/login');
+    } else {
+        next();
+    }
+};
+
+module.exports = withAuth;
